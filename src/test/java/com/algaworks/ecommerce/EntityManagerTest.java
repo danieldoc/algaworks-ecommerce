1fb9ac86dd25ab4b61de4df0ea5818825555dd5c
Daniel <daniel.olicruz13@gmail.com>
2020-11-11 20:39:33 -0300

    Criado teste insercao com transacao

--- src/test/java/com/algaworks/ecommerce/EntityManagerTest.java
+++ src/test/java/com/algaworks/ecommerce/EntityManagerTest.java
@@ -1,4 +1,4 @@
-package com.algaworks.ecommerce.iniciandocomjpa;
+package com.algaworks.ecommerce;
 
 import org.junit.After;
 import org.junit.AfterClass;
--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
@@ -1,6 +1,6 @@
-package com.algaworks.ecommerce;
+package com.algaworks.ecommerce.iniciandocomjpa;
 
-import com.algaworks.ecommerce.iniciandocomjpa.EntityManagerTest;
+import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Produto;
 import org.junit.Assert;
 import org.junit.Test;
