a3295d2c5d7a648b9f8873f70c7176d5b0ee137b
Daniel <daniel.olicruz13@gmail.com>
2020-11-11 20:19:56 -0300

    Criado classe generica entityManagerTest

---
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/EntityManagerTest.java
@@ -0,0 +1,37 @@
+package com.algaworks.ecommerce.iniciandocomjpa;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EntityManagerTest {
+
+    protected static EntityManagerFactory entityManagerFactory;
+
+    protected EntityManager entityManager;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        entityManagerFactory = Persistence.createEntityManagerFactory("Ecommerce-PU");
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        entityManagerFactory.close();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        entityManager.close();
+    }
+}
