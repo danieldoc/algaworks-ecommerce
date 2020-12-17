dd22d8c7e3c35416940020766d7d9ed51e315988
Daniel <daniel.olicruz13@gmail.com>
2020-12-17 18:46:58 -0300

    Mapeado OneToMany itens do Pedido

--- src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentoOneToManyTest.java
+++ src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentoOneToManyTest.java
@@ -1,9 +1,7 @@
 package com.algaworks.ecommerce.relacionamentos;
 
 import com.algaworks.ecommerce.EntityManagerTest;
-import com.algaworks.ecommerce.model.Cliente;
-import com.algaworks.ecommerce.model.Pedido;
-import com.algaworks.ecommerce.model.StatusPedido;
+import com.algaworks.ecommerce.model.*;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,7 +9,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class RelacionamentoOneToManyTest extends EntityManagerTest {
-    
+
     @Test
     public void verificarRelacionamento() {
         Cliente cliente = entityManager.find(Cliente.class, 1);
@@ -30,5 +28,33 @@ public class RelacionamentoOneToManyTest extends EntityManagerTest {
 
         Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
         Assert.assertFalse(clienteVerificacao.getPedidos().isEmpty());
+    }
+
+    @Test
+    public void verificarRelacionamentoItemPedido() {
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+        Produto produto = entityManager.find(Produto.class, 1);
+
+        Pedido pedido = new Pedido();
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+        pedido.setDataPedido(LocalDateTime.now());
+        pedido.setTotal(BigDecimal.TEN);
+        pedido.setCliente(cliente);
+
+        ItemPedido item = new ItemPedido();
+        item.setPrecoProduto(produto.getPreco());
+        item.setQuantidade(1);
+        item.setPedido(pedido);
+        item.setProduto(produto);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pedido);
+        entityManager.persist(item);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, item.getId());
+        Assert.assertFalse(pedidoVerificacao.getItens().isEmpty());
     }
 }
