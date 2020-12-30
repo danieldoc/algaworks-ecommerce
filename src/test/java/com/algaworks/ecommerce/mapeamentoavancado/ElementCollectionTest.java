b7236926bcc61d835ca130c41232ea8b8a822278
Daniel <daniel.olicruz13@gmail.com>
2020-12-29 22:49:23 -0300

    Adicionado atributo contatos do Cliente Map com ElementCollection

--- src/test/java/com/algaworks/ecommerce/mapeamentoavancado/ElementCollectionTest.java
+++ src/test/java/com/algaworks/ecommerce/mapeamentoavancado/ElementCollectionTest.java
@@ -2,6 +2,7 @@ package com.algaworks.ecommerce.mapeamentoavancado;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Atributo;
+import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Produto;
 import org.junit.Assert;
 import org.junit.Test;
@@ -39,5 +40,21 @@ public class ElementCollectionTest extends EntityManagerTest {
 
         final Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
         Assert.assertFalse(produtoVerificacao.getAtributos().isEmpty());
+    }
+
+    @Test
+    public void aplicarContatos() {
+        entityManager.getTransaction().begin();
+
+        final Cliente cliente = entityManager.find(Cliente.class, 1);
+        cliente.setContatos(Collections.singletonMap("email", "fernando@email.com"));
+
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        final Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+        Assert.assertFalse(clienteVerificacao.getContatos().isEmpty());
+        Assert.assertEquals("fernando@email.com", clienteVerificacao.getContatos().get("email"));
     }
 }
