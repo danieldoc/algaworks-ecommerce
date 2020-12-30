f757519b297ec16c9f39ab71f3faeac116be5f6e
Daniel <daniel.olicruz13@gmail.com>
2020-12-29 22:48:50 -0300

    Criado atributo foto do Produto com Lob

--- src/test/java/com/algaworks/ecommerce/mapeamentoavancado/SalvaArquivosTest.java
+++ src/test/java/com/algaworks/ecommerce/mapeamentoavancado/SalvaArquivosTest.java
@@ -3,6 +3,7 @@ package com.algaworks.ecommerce.mapeamentoavancado;
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.NotaFiscal;
 import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Produto;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -31,7 +32,7 @@ public class SalvaArquivosTest extends EntityManagerTest {
 
         entityManager.clear();
 
-        final NotaFiscal notaFiscalVerificacao = entityManager.find(NotaFiscal.class, 1);
+        final NotaFiscal notaFiscalVerificacao = entityManager.find(NotaFiscal.class, notaFiscal.getId());
         Assert.assertNotNull(notaFiscalVerificacao);
         Assert.assertNotNull(notaFiscalVerificacao.getXml());
         Assert.assertTrue(notaFiscalVerificacao.getXml().length > 0);
@@ -44,9 +45,35 @@ public class SalvaArquivosTest extends EntityManagerTest {
 //        }
     }
 
+    @Test
+    public void salvarFotoProduto() {
+        entityManager.getTransaction().begin();
+        final Produto produto = entityManager.find(Produto.class, 1);
+
+        produto.setFoto(carregarFoto());
+
+        entityManager.persist(produto);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        final Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assert.assertNotNull(produtoVerificacao);
+        Assert.assertNotNull(produtoVerificacao.getFoto());
+        Assert.assertTrue(produtoVerificacao.getFoto().length > 0);
+    }
+
     public byte[] carregarXml() {
         try {
             return SalvaArquivosTest.class.getResourceAsStream("/nota-fiscal.xml").readAllBytes();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public byte[] carregarFoto() {
+        try {
+            return SalvaArquivosTest.class.getResourceAsStream("/produto.jpg").readAllBytes();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
