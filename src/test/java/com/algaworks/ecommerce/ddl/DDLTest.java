351d4971ec96b26fe96696ad382ad0b90da62220
Daniel <daniel.olicruz13@gmail.com>
2020-12-30 20:38:13 -0300

    Configurado indexes e unique constraints para Cliente

---
+++ src/test/java/com/algaworks/ecommerce/ddl/DDLTest.java
@@ -0,0 +1,12 @@
+package com.algaworks.ecommerce.ddl;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import org.junit.Test;
+
+public class DDLTest extends EntityManagerTest {
+
+    @Test
+    public void gerarDDL() {
+//        Somente para exibicao da criacao de index do Cliente no console
+    }
+}
